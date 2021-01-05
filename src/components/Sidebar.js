8d8dc5a35069c5b51aa6fa27b0d09fe8adc492ff
Chester Cari <chester_cari@outlook.com>
2021-01-05 14:32:17 -0500

    fixed sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,9 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
-// import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import ColorPicker from './ColorPicker';
-// import { SidebarData } from './SidebarData';
-// import SubMenu from './SubMenu';
-// import { IconContext } from 'react-icons/lib';
 
 const Nav = styled.div`
   background: #15171c;
@@ -50,10 +46,9 @@ const Sidebar = () => {
 
     return (
         <>
-            {/* <IconContext.Provider value={{ color: '#fff' }}> */}
+
             <Nav>
                 <NavIcon to='#'>
-                    {/* <div onClick={showSidebar}>Hello</div> */}
                     <FaIcons.FaBars onClick={showSidebar} />
                 </NavIcon>
 
@@ -61,16 +56,11 @@ const Sidebar = () => {
             <SidebarNav sidebar={sidebar}>
                 <SidebarWrap>
                     <NavIcon to='#'>
-                        {/* <div onClick={showSidebar}>close</div> */}
                         <AiIcons.AiOutlineClose onClick={showSidebar} />
                     </NavIcon>
                     <ColorPicker />
-                    {/* {SidebarData.map((item, index) => {
-              return <SubMenu item={item} key={index} />;
-            })} */}
                 </SidebarWrap>
             </SidebarNav>
-            {/* </IconContext.Provider> */}
         </>
     );
 };
